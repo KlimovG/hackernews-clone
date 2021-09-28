e53841dc2354c32da91c0e3deae4669eb36d9a67
Micha <micha.pdm@gmx.de>
2021-09-28 19:47:14 +0200

    feat: draft to fetch data via API

--- src/App.js
+++ src/App.js
@@ -2,27 +2,14 @@ import 'normalize.css';
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 import Header from './Components/header/header'
+import TestOutput from './TestOutput';
 
 function App() {
   return (
-
+    <>
     <Header />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <TestOutput />
+    </>
   );
 }
 
