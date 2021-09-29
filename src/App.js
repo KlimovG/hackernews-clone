5f6991325ce4d640bd11835bdee1602675478bca
George <klimovgg@gmail.com>
2021-09-29 16:07:25 +0200

    create function to take value from search input

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
+import React, { useState } from 'react';
 import 'normalize.css';
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 import Header from './Components/header/header'
+import Article from './Components/Article/Article'
 
 function App() {
-  return (
+  const [searchValue, setSearchValue] = useState("")
 
-    <Header />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+  const getSearchValue = (e) => {
+    setSearchValue(prev => prev = e.target.value)
+  }
+  const onSearch = (value) => {
+    value = searchValue;
+    console.log(value)
+    fetch(`http://hn.algolia.com/api/v1/search?query=${value}`)
+      .then(
+        (response) => {
+          // console.log(response);
+          if (!response.ok)
+            // Failed HTTP status
+            throw new Error(
+              `An error has occured during the request. HTTP status code: ${response.status}`
+            );
+          return response.json();
+        },
+        (error) => {
+          console.log("Rejection error callback");
+          console.log(error);
+        }
+      )
+      .then((data) => {
+        console.log(data);
+        setSearchValue(prev => prev = "")
+      })
+      .catch((error) => {
+        console.log("Catch block");
+        console.log(error);
+      });
+
+  }
+  return (
+    <>
+      <Header isValue={getSearchValue} onSearch={onSearch} value={searchValue} />
+      <main>
+        <div className="container">
+          <ol className="article-list">
+            <Article />
+            <Article />
+
+          </ol>
+        </div>
+      </main>
+    </>
   );
 }
 
